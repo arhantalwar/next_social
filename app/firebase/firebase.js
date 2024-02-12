208b5d5bbb1b8600b46cfb04afbe83fb4c325f55
arhantalwar <arhantalwar@gmail.com>
2024-02-13 00:27:02 +0530

    config

--- app/firebase/firebase.js
+++ app/firebase/firebase.js
@@ -2,12 +2,12 @@ import { getApps, initializeApp, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBLn5wotHF6VAfyPOrTXrc-Rtkbqkx0BHY",
-    authDomain: "next-firebase-56395.firebaseapp.com",
-    projectId: "next-firebase-56395",
-    storageBucket: "next-firebase-56395.appspot.com",
-    messagingSenderId: "1004447138758",
-    appId: "1:1004447138758:web:00738fa643726fbd0ee8c7"
+    apiKey: process.env.APIKEY, 
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        APIKEY: "AIzaSyBLn5wotHF6VAfyPOrTXrc-Rtkbqkx0BHY",
+        AUTHDOMAIN: "next-firebase-56395.firebaseapp.com",
+        PROJECTID: "next-firebase-56395",
+        STORAGEBUCKET: "next-firebase-56395.appspot.com",
+        MESSAGINGSENDERID: "1004447138758",
+        APPID: "1:1004447138758:web:00738fa643726fbd0ee8c7"
+    }
+};
 
 export default nextConfig;
